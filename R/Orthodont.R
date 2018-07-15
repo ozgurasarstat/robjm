06f73ab38955bb7498e8fae169542838d6ce2c86
ozgurasarstat <ozgurasarstat@gmail.com>
2018-07-15 15:32:01 +0300

    documentation

---
+++ R/Orthodont.R
@@ -0,0 +1,12 @@
+#' Orthodont data-set.
+#'
+#' A dataset containing dental data for 27 children at ages 8, 10, 12, 14
+#'
+#' @format A data frame with 108 rows and 4 variables:
+#' \describe{
+#'   \item{distance}{outcome}
+#'   \item{age}{age in years}
+#'   \item{Subject}{id}
+#'   \item{Sex}{sex of the child}
+#' }
+"Orthodont"
