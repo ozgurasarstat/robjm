cd16866b3028d90df8569d6f9f01cb61f2d2b99f
ozgurasarstat <ozgurasarstat@gmail.com>
2018-07-15 15:41:56 +0300

    documentation

---
+++ R/aids.R
@@ -0,0 +1,17 @@
+#' AIDS data-set - longitudinal.
+#'
+#' A dataset containing data for 467 patients
+#'
+#' @format A data frame with 1405 rows and 9 variables:
+#' \describe{
+#'   \item{patient}{id}
+#'   \item{Time}{survival time}
+#'   \item{death}{died? 1 = yes}
+#'   \item{CD4}{biomarker}
+#'   \item{obstime}{follow-up time}
+#'   \item{drug}{drug group}
+#'   \item{gender}{sex}
+#'   \item{prevOI}{previously had AIDS?}
+#'   \item{AZT}{intolerant to AZT?}
+#' }
+"aids"
---
+++ R/aids.id.R
@@ -0,0 +1,17 @@
+#' AIDS data-set - baseline.
+#'
+#' A dataset containing data for 467 patients
+#'
+#' @format A data frame with 467 rows and 9 variables:
+#' \describe{
+#'   \item{patient}{id}
+#'   \item{Time}{survival time}
+#'   \item{death}{died? 1 = yes}
+#'   \item{CD4}{biomarker}
+#'   \item{obstime}{follow-up time}
+#'   \item{drug}{drug group}
+#'   \item{gender}{sex}
+#'   \item{prevOI}{previously had AIDS?}
+#'   \item{AZT}{intolerant to AZT?}
+#' }
+"aids.id"
