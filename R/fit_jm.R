69bd34fce036f0b3002d68eaae73ff82b96c7c5d
ozgurasarstat <ozgurasarstat@gmail.com>
2018-07-15 20:55:26 +0300

    some updates

--- R/fit_jm.R
+++ R/fit_jm.R
@@ -49,17 +49,14 @@ fit_jm <- function(fixed_long,
   
   t_quad <- 0.5 * rep(S, each = Q) * (1 + rep(pt, ngroup))
   
-  #####
-  #####
   ncol_e <- 2
-  e <- cbind(ifelse(S < 10, 1, 0), ifelse(S >= 10, 1, 0))#bs(S, df = 3)
+  median_S <- median(S)
+  e <- cbind(ifelse(S < median_S, 1, 0), ifelse(S >= median_S, 1, 0))#bs(S, df = 3)
   #attributes(e) <- NULL
   #e <- matrix(e, ncol = 3)
-  e_quad <- cbind(ifelse(t_quad < 10, 1, 0), ifelse(t_quad >= 10, 1, 0))#bs(t_quad, df = 3)
+  e_quad <- cbind(ifelse(t_quad < median_S, 1, 0), ifelse(t_quad >= median_S, 1, 0))#bs(t_quad, df = 3)
   #attributes(e_quad) <- NULL
   #e_quad <- matrix(e_quad, ncol = 3)
-  #####
-  #####
   
   c <- model.matrix(cbind(a, b) ~ c, data)[, -1, drop = FALSE]
   ncol_c <- ncol(c)
@@ -80,48 +77,49 @@ fit_jm <- function(fixed_long,
   priors_long <- c(5, 2, 5, 5, 5)
   priors_surv <- c(5, 5, 5)
   
-  a <- splines::ns(long_data[, "obstime"], df = 3)
-  ncol_a <- ncol(a)
-  attributes(a) <- NULL
-  a <- matrix(a, ncol = ncol_a)
-  
-  s <- ncol_a
-  
-  data_nor_nor_jm <- list(ntot = ntot,
-                          id = id, 
-                          y = y, 
-                          p = p,
-                          q = q,
-                          ngroup = ngroup, 
-                          x = x, 
-                          d = d,
-                          priors_long = priors_long,
-                          priors_surv = priors_surv,
-                          Q = Q,
-                          ntot_quad = ntot_quad,
-                          S = S,
-                          E = E,
-                          ncol_e = ncol_e, 
-                          e = e, 
-                          e_quad = e_quad,
-                          ncol_c = ncol_c, 
-                          c = c, 
-                          c_quad = c_quad,
-                          x_T = x_T,
-                          x_quad = x_quad,
-                          d_T = d_T,
-                          d_quad = d_quad,
-                          wt_quad = wt_quad,
-                          s = s,
-                          a = a
-  )
-  
-  res <- stan(model_code = t_t_tv_jm,
-              data = data_nor_nor_jm,
-              ...
-              )
+  if(model == "t_t_tv"){
+    a <- splines::ns(long_data[, "obstime"], df = 3)
+    ncol_a <- ncol(a)
+    attributes(a) <- NULL
+    a <- matrix(a, ncol = ncol_a)
+    s <- ncol_a
+    
+    data_t_t_tv <- list(ntot = ntot,
+                        id = id, 
+                        y = y, 
+                        p = p,
+                        q = q,
+                        ngroup = ngroup, 
+                        x = x, 
+                        d = d,
+                        priors_long = priors_long,
+                        priors_surv = priors_surv,
+                        Q = Q,
+                        ntot_quad = ntot_quad,
+                        S = S,
+                        E = E,
+                        ncol_e = ncol_e, 
+                        e = e, 
+                        e_quad = e_quad,
+                        ncol_c = ncol_c, 
+                        c = c, 
+                        c_quad = c_quad,
+                        x_T = x_T,
+                        x_quad = x_quad,
+                        d_T = d_T,
+                        d_quad = d_quad,
+                        wt_quad = wt_quad,
+                        s = s,
+                        a = a
+    )
+    
+    res <- stan(model_code = t_t_tv_jm,
+                data = data_nor_nor_jm,
+                ...
+    )
+  }
+
   return(res)  
   
-    
 }
 
