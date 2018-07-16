1b9962da4f8b6fe155c0c8f2cbfc71e96e4abc3f
ozgurasarstat <ozgurasarstat@gmail.com>
2018-07-16 18:50:08 +0300

    some fixes

--- R/fit_jm.R
+++ R/fit_jm.R
@@ -29,26 +29,32 @@ fit_jm <- function(fixed_long,
   id_dmat_list <- lapply(split(id_dmat[, -1], id_dmat[, 1]), as.matrix)
   d <- do.call(magic::adiag, id_dmat_list)
   
+  ## total number of observations in the longitudinal data
+  ## number of covariates in the x and d matrices
   ntot <- nrow(x) 
   p <- ncol(x)
   q <- ncol(id_dmat) - 1
   
-  ngroup <- length(unique(id_long))
+  ## number of subjects
+  ngroup <- length(unique(data_long[, id_long]))
   
-  x <- cbind(1, data_long$obstime)
-  
+  ## Gauss - Legendre weights and abscissas
   gl_quad <- statmod::gauss.quad(Q)
   wt <- gl_quad$weights
   pt <- gl_quad$nodes
   
+  ## total number of observations for quadrature approx.
   ntot_quad <- ngroup * Q
   
+  ## extract survival times and event indicator
   mf_surv <- model.frame(fixed_surv, data_surv)
   S <- mf_surv[, 1]
   E <- mf_surv[, 2]
   
+  ## calculate times for hazard function for quadrature approx
   t_quad <- 0.5 * rep(S, each = Q) * (1 + rep(pt, ngroup))
   
+  ## baseline hazard - piecewise constant with one know a.t.m. 
   ncol_e <- 2
   median_S <- median(S)
   e <- cbind(ifelse(S < median_S, 1, 0), ifelse(S >= median_S, 1, 0))#bs(S, df = 3)
@@ -58,13 +64,16 @@ fit_jm <- function(fixed_long,
   #attributes(e_quad) <- NULL
   #e_quad <- matrix(e_quad, ncol = 3)
   
+  ## fixed effects for survival sub-model
   c <- model.matrix(fixed_surv, data_surv)[, -1, drop = FALSE]
   ncol_c <- ncol(c)
   c_quad <- apply(c, 2, function(i) rep(i, each = Q))
   
-  x_T <- cbind(1, S)
+  ## x matrix for log survival density
+  x_T <- cbind(1, S) 
   x_quad <- cbind(1, t_quad)
   
+  ## d matrix for log survival density
   id_dmat_T <- data.frame(data_surv[, id_surv], cbind(1, S))
   id_dmat_list_T <- lapply(split(id_dmat_T[, -1], id_dmat_T[, 1]), as.matrix)
   d_T <- do.call(magic::adiag, id_dmat_list_T)
@@ -73,7 +82,10 @@ fit_jm <- function(fixed_long,
   id_dmat_list_quad <- lapply(split(id_dmat_quad[, -1], id_dmat_quad[, 1]), as.matrix)
   d_quad <- do.call(magic::adiag, id_dmat_list_quad)
   
+  ## extend the weights for quadrature approx.
   wt_quad <- rep(wt, ngroup)
+  
+  ## prior hyperparameters
   priors_long <- c(5, 2, 5, 5, 5)
   priors_surv <- c(5, 5, 5)
   
