3bd8c5ddc27ea97799c48a467fc7a616dc2dcafb
Ozgur Asar <ozgurasarstat@gmail.com>
2018-12-07 13:17:25 +0300

    efficient

--- R/t_t_tv_jm.R
+++ R/t_t_tv_jm.R
@@ -52,7 +52,8 @@ vector[q] zero_B = rep_vector(0, q);
 parameters{
 //longitudinal sub-model
 vector[p] alpha;              // fixed effects coefficients
-matrix[ngroup, q] Bstar;          // random effects coefficients
+//matrix[ngroup, q] Bstar;          // random effects coefficients
+vector[q] Bstar[ngroup];
 corr_matrix[q] Omega;         // correlation matrix for random effects
 vector<lower = 0>[q] sigma_B; // scale parameters for random effects
 real<lower = 0> sigma_Z;      // scale parameter of measurement error
@@ -70,14 +71,14 @@ real eta;             // association parameter
 
 transformed parameters{
 cov_matrix[q] Sigma; 
-vector[ntot] linpred;
+//vector[ntot] linpred;
 matrix[ngroup, q] B;         
 //matrix[ngroup * q, 1] Bmat;
 vector[ntot] d_B;
 vector[ngroup] d_T_B;
 vector[ntot_quad] d_quad_B;
-real<lower = 2, upper = 100> phi;
-real<lower = 2, upper = 100> delta0;
+//real<lower = 2, upper = 100> phi;
+//real<lower = 2, upper = 100> delta0;
 vector<lower = 0>[ntot] delta;
 
 vector[ngroup] lsd_expr1;
@@ -93,25 +94,28 @@ vector[ngroup] lsd;
 
 //longitudinal sub-model
 
-phi = 1/phi_inv;
-delta0 = 1/delta0_inv;
+//phi = 1/phi_inv;
+//delta0 = 1/delta0_inv;
 
-delta = to_vector(delta0 * exp(a * beta));
+delta = to_vector((1/delta0_inv) * exp(a * beta));
 
-for(i in 1:ngroup){
-B[i, ] = Bstar[i, ] * sqrt(V[i]);
+//for(i in 1:ngroup){
+//B[i, ] = Bstar[i, ] * sqrt(V[i]);
+//}
+for(i in 1:q){
+B[, i] = to_vector(Bstar[, i]) .* sqrt(V);
 }
 
 //Bmat = to_matrix(B', ngroup * q, 1);
 
 for(i in 1:ngroup){
-d_B[d_ind[i, 1]:d_ind[i, 2]] = to_vector(d[d_ind[i, 1]:d_ind[i, 2], ] * to_matrix(B[i, ], q, 1));
-d_T_B[i:i] = to_vector(d_T[i, ] * to_matrix(B[i, ], q, 1));
-d_quad_B[Q_ind[i, 1]:Q_ind[i, 2]] = to_vector(d_quad[Q_ind[i, 1]:Q_ind[i, 2], ] * to_matrix(B[i, ], q, 1));
+d_B[d_ind[i, 1]:d_ind[i, 2]] = d[d_ind[i, 1]:d_ind[i, 2], ] * to_vector(B[i]);
+d_T_B[i] = sum(d_T[i] .* B[i]);
+d_quad_B[Q_ind[i, 1]:Q_ind[i, 2]] = d_quad[Q_ind[i, 1]:Q_ind[i, 2], ] * to_vector(B[i]);
 }
 
 //linpred = x * alpha + to_vector(d * Bmat);
-linpred = x * alpha + d_B;
+//linpred = x * alpha + d_B;
 
 Sigma = quad_form_diag(Omega, sigma_B);
 
@@ -121,14 +125,18 @@ lsd_expr1_fix = c * omega;
 //lsd_expr1_ystar = x_T * alpha + to_vector(d_T * Bmat);
 lsd_expr1_ystar = x_T * alpha + d_T_B;
 
-lsd_expr1 = E .* (lsd_expr1_spl + lsd_expr1_fix + rep_vector(eta, ngroup) .* lsd_expr1_ystar);
+lsd_expr1 = E .* (lsd_expr1_spl + 
+                  lsd_expr1_fix + 
+                  eta * lsd_expr1_ystar);
 
 lsd_expr2_quad_spl = e_quad * zeta; 
 lsd_expr2_quad_fix = c_quad * omega; 
 //lsd_expr2_quad_ystar = x_quad * alpha + to_vector(d_quad * Bmat); 
 lsd_expr2_quad_ystar = x_quad * alpha + d_quad_B; 
 
-lsd_expr2_quad = wt_quad .* exp(lsd_expr2_quad_spl + lsd_expr2_quad_fix + rep_vector(eta, ntot_quad) .* lsd_expr2_quad_ystar);
+lsd_expr2_quad = wt_quad .* exp(lsd_expr2_quad_spl + 
+                                lsd_expr2_quad_fix + 
+                                eta * lsd_expr2_quad_ystar);
 
 for(i in 1:ngroup){
 lsd_expr2[i] = 0.5 * S[i] * sum(lsd_expr2_quad[Q_ind[i, 1]:Q_ind[i, 2]]);
@@ -140,25 +148,22 @@ lsd = lsd_expr1 - lsd_expr2;
 
 model{
 
-alpha ~ cauchy(0, priors_long[1]);
+alpha[1] ~ cauchy(0, priors_long[1] * 4);
+for(i in 2:p) alpha[i] ~ cauchy(0, priors_long[1]);
 
-for(i in 1:ngroup){
-Bstar[i] ~ multi_normal(zero_B, Sigma);
-}
+Bstar ~ multi_normal(zero_B, Sigma);
 
 Omega ~ lkj_corr(priors_long[2]);
 sigma_B ~ cauchy(0, priors_long[3]);
 sigma_Z ~ cauchy(0, priors_long[4]);
 
-V ~ inv_gamma(phi/2, phi/2);
+V ~ inv_gamma((1/phi_inv)/2, (1/phi_inv)/2);
 //phi_inv ~ uniform(0.01, 0.5);//the prior is uniform with -infty and infty, constrained above
-for(i in 1:ntot){
-W[i] ~ inv_gamma(delta[i]/2, delta[i]/2);
-}
+W ~ inv_gamma(delta/2, delta/2);
 beta ~ cauchy(0, priors_long[5]);
 //prior for delta0_inv is uniform -infty, infty; constrained above
 
-for(i in 1:ntot) y[i] ~ normal(linpred[i], sigma_Z * sqrt(W[i]));
+y ~ normal(x * alpha + d_B, sigma_Z * sqrt(W));
 
 zeta ~ cauchy(0, priors_surv[1]);
 omega ~ cauchy(0, priors_surv[2]);
