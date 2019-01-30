75d30a550de779b540968223506d5084b86a7941
Ozgur Asar <ozgurasarstat@gmail.com>
2019-01-30 11:19:00 +0300

    prep_data_invd_pred put into util

--- R/util.R
+++ R/util.R
@@ -96,3 +96,28 @@ stime_event_fun <- function(x, t_max, a){
     matrix(c(max(x), 0), ncol = 2)[rep(1, length(x)), ]
   }
 }
+
+# function to create a data frame for individual predictions -- useful for parallel computing
+
+prep_data_indv_pred <- function(data, id, timeVar){
+  
+  id_unique <- unique(data[, id])
+  
+  data_out <- data.frame()
+  
+  for(i in id_unique){
+    
+    data_i <- data[data[, id] == i, ]
+    
+    for(j in 1:nrow(data_i)){
+      data_i_j <- data_i[1:j, ]
+      data_i_j[, id] <- paste0(i, "_", data_i_j[nrow(data_i_j), timeVar]) %>% rep(nrow(data_i_j))
+      data_out <- rbind(data_out, data_i_j)
+    }
+    
+  }
+  
+  return(data_out)
+  
+}
+
